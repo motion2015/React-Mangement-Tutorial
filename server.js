09e73e8c07c9cf1dd48961f017d110d30f9ff0ac
nosidbar <nosidemean@gmail.com>
2022-05-22 20:19:34 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,49 +8,27 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql =  require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '대학생',
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '홍길민',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '대학생',
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '배현정',
-      'birthday': '961222',
-      'gender': '여자',
-      'job': '동료',
-    },
-    {
-      'id': 4,
-      'image': 'https://placeimg.com/64/64/4',
-      'name': '박민숙',
-      'birthday': '961222',
-      'gender': '여자',
-      'job': '나',
-    },
-    {
-      'id': 5,
-      'image': 'https://placeimg.com/64/64/5',
-      'name': '제나영',
-      'birthday': '961222',
-      'gender': '여자',
-      'job': '친구',
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
     }
-  ]);
+  )
 })
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
