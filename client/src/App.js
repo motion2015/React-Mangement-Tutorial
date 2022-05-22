0020a0cfc8399f1f7bbced6ba3d8481480327dab
nosidbar <nosidemean@gmail.com>
2022-05-22 18:25:28 +0900

    Add Customers API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import TableHead from '@mui/material/TableHead';
 import {makeStyles} from '@mui/styles';
 import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const useStyles = makeStyles({
   root: {
@@ -18,6 +22,9 @@ const useStyles = makeStyles({
   },
   table : {
     minWidth: 1080
+  },
+  progress: {
+    margin: '8px'
   }
 })
 /* const customer = [
@@ -65,15 +72,56 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   const [customers, setCustomers] = useState([]);
+  const [progress, setProgress] = useState(0);
   useEffect(()=> {
     callApi()
-    .then(res => setCustomers(res))      
+    .then(res => setCustomers(res))
+    .catch(err => console.log(err));
+
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 20));
+    }, 20);
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   const callApi = async() =>{
      const response = await fetch('/api/customers');
      const body = await response.json();
      return body;
+  };
+
+  function CircularProgressWithLabel(props) {
+    return (
+      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="caption" component="div" color="text.secondary">
+            {`${Math.round(props.value)}%`}
+          </Typography>
+        </Box>
+      </Box>
+    );
+  }
+  CircularProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate variant.
+     * Value between 0 and 100.
+     * @default 0
+     */
+    value: PropTypes.number.isRequired,
   };
 
   return (
@@ -91,9 +139,13 @@ function App() {
         </TableHead>
         <TableBody>
         {
-        customers? customers.map(c=> {
+        customers.length>0?  customers.map(c=> {
           return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-        }) : ""
+        }) : (<TableRow>
+          <TableCell  colSpan="6" align="center">
+          <CircularProgressWithLabel value={progress} />
+          </TableCell>        
+      </TableRow>)
       }
         </TableBody>
       </Table>
